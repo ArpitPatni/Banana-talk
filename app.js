a86448e7eab5c8a2261a3f849b6259faabe664b8
ArpitPatni <arpitpatni00@gmail.com>
2021-09-23 18:52:53 +0530

    feat:server added for translation

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
 
 
+var serverURL="https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslationURL(text){
+    return serverURL + "?" +"text=" +text;
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("Something went wrong with server. Try again after sometime !!");
+}
 
 function clickEventHandler() {
-    console.log("Clicked !!")
-    console.log("input", txtInput.value);
-    outputDiv.innerText="hello "+txtInput.value;
+    var inputText=txtInput.value; //here i am taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response =>response.json)
+    .then(json=>{
+        var translatedText=json.contents.translated;
+        outputDiv.innerText= translatedText; // showing output
+    })
+    .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", clickEventHandler)
