d9bed3a60a394d473c805ef016f3f5c26bd6a8e2
ArpitPatni <arpitpatni00@gmail.com>
2021-09-23 16:37:10 +0530

    feat: input output processing done

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+
+
 
 function clickEventHandler() {
     console.log("Clicked !!")
     console.log("input", txtInput.value);
+    outputDiv.innerText="hello "+txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickEventHandler)
